99c5462418e92860941c90b1755fa5034c22e98d
Jorgenssen <n.tatyannikov@gmail.com>
2021-05-16 15:01:50 +0100

    + lombok

--- src/main/java/org/example/UserContact.java
+++ src/main/java/org/example/UserContact.java
@@ -1,32 +1,12 @@
 package org.example;
 
-public class UserContact {
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
+public class UserContact {
     private String id;
     private String name;
     private String phoneNumber;
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
 }
